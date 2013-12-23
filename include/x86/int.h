83f058163ba2a53a4ec15258db4b9e67d5a30e6c
Ying-Shiuan Pan <yingshiuan.pan@gmail.com>
2013-12-23 23:03:20 +0800

    int.h: make enable_int() and disable_int() static
    prevent duplicated difinition

--- include/x86/int.h
+++ include/x86/int.h
@@ -1,12 +1,12 @@
 #ifndef __X86_INT_H__
 #define __X86_INT_H__
 
-inline void disable_int()
+static inline void disable_int()
 {
 	__asm__  __volatile__ ("cli");
 }
 
-inline void enable_int()
+static inline void enable_int()
 {
 	__asm__  __volatile__ ("sti");
 }
